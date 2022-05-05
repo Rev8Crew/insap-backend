fa95cda8d9ec6734fe0dfb73acfc3d556225efbf
SheeeeshMaster <dimaelik71@gmail.com>
2022-05-05 20:57:52 +0300

    rewrite importer to process

---
+++ database/migrations/2022_04_13_170307_create_processes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProcessesTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('processes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            //
+            $table->string('name')->nullable();
+            $table->string('description')->nullable();
+
+            $table->integer('type'); // Import | Export
+            $table->string('interpreter')->comment('Interpreter like PHP, python, go ...')->nullable();
+
+            $table->integer('appliance_id')->index()->nullable();
+            $table->integer('plugin_id')->index()->nullable();
+            $table->integer('user_id')->index()->nullable();
+
+            $table->integer('is_active');
+
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('processes');
+    }
+}
